67bb48c53b2c3a17d1bc00b7f7aecc9218354308
Ethan Nguyen <etnguyen16@gmail.com>
2022-11-09 14:11:34 -0700

    add and update routes

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+         get '/subscriptions', to: 'subscriptions#index'
+         post '/subscriptions', to: 'subscriptions#create'
+         patch '/subscriptions', to: 'subscriptions#update'
+    end
+  end
+#   namespace :api do 
+#     namespace :v1 do 
+
+#       patch '/subscriptions/cancel', to: 'subscriptions#update' 
+
+#       get '/customer/subscriptions', to: 'subscriptions#index'
+# # make index a customer resource 
+#       resources :subscriptions, only: %i[create] 
+#     end
+#   end
+end
+
