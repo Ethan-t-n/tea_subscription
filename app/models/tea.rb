c8c1f135fcf34d1eb5841d7f8b9ddcc58fed5b72
Ethan Nguyen <etnguyen16@gmail.com>
2022-11-09 14:09:25 -0700

    add tea model

---
+++ app/models/tea.rb
@@ -0,0 +1,8 @@
+class Tea < ApplicationRecord
+  validates_presence_of :title, :description, :temperature, :brew_time
+
+  has_many :subscriptions
+  has_many :customers, through: :subscriptions
+
+end
+
